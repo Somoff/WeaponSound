ef22539e492cfb781233fe2dce13b3aaf45fc4ed
ImperatorZ <asassins7770@gmail.com>
2015-08-12 10:54:48 +0300

    Update

---
+++ app/src/main/java/com/example/myproject/ChooseWeapons.java
@@ -0,0 +1,7 @@
+package com.example.myproject;
+
+/**
+ * Created by Somoff on 12.08.2015.
+ */
+public class ChooseWeapons  {
+}
---
+++ app/src/main/java/com/example/myproject/Start.java
@@ -0,0 +1,64 @@
+package com.example.myproject;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+/**
+ * Created by Somoff on 11.08.2015.
+ */
+public class Start extends Activity {
+    String url = "https://vk.com/som0ff";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_layout);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    public void OnClickBrouser(View view) {
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+
+    }
+
+    public void OnClickExit(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+                .setTitle("Выход")
+                .setMessage("Вы точно хотите выйти?")
+                .setCancelable(false)
+                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Спасибо вам", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Хнык", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        finish();
+                        System.exit(0);
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+}
