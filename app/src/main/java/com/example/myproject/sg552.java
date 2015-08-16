f0d94b44adf70fdd0eb4828565702fbfce41fd19
ImperatorZ <asassins7770@gmail.com>
2015-08-16 18:12:38 +0300

    Nanananananana

---
+++ app/src/main/java/com/example/myproject/sg552.java
@@ -0,0 +1,156 @@
+package com.example.myproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Somoff on 16.08.2015.
+ */
+public class sg552 extends Allmusicgun implements View.OnTouchListener {
+    Timer timer;
+    String[] data = {"AK47", "AK74", "AUG", "M4A1", "FAMAS", "FS2000", "G36C", "SG552"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sg552);
+        patron1 = (TextView) findViewById(R.id.sg552view);
+        patron1.setText(String.valueOf(patron));
+        ImageView im3 = (ImageView) findViewById(R.id.sg552button);
+        im3.setOnTouchListener(this);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        android.widget.Spinner spinner = (android.widget.Spinner) findViewById(R.id.spinner4sg552);
+        spinner.setAdapter(adapter);
+
+        spinner.setSelection(7, true);
+
+        adapter.notifyDataSetChanged();
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                CustomAdapter.flag = true;
+
+                if (position == 0) {
+                    Toast.makeText(sg552.this, "Weapon Selected AK47", Toast.LENGTH_SHORT).show();
+                    Intent ak47 = new Intent(getApplicationContext(), ak47.class);
+                    startActivity(ak47);
+                }
+                if (position == 1) {
+                    Toast.makeText(sg552.this, "Weapon Selected AK74", Toast.LENGTH_SHORT).show();
+                    Intent ak74 = new Intent(getApplicationContext(), ak74.class);
+                    startActivity(ak74);
+                }
+                if (position == 2) {
+                    Toast.makeText(sg552.this, "Weapon Selected AUG", Toast.LENGTH_SHORT).show();
+                    Intent aug = new Intent(getApplicationContext(), aug_activity.class);
+                    startActivity(aug);
+                }
+                if (position == 3) {
+                    Toast.makeText(sg552.this, "Weapon Selected M4A1", Toast.LENGTH_SHORT).show();
+                    Intent m4a1 = new Intent(getApplicationContext(), m4a1.class);
+                    startActivity(m4a1);
+
+                }
+                if (position == 4) {
+                    Toast.makeText(sg552.this, "Weapon Selected FAMAS", Toast.LENGTH_SHORT).show();
+                    Intent famas = new Intent(getApplicationContext(), Famas.class);
+                    startActivity(famas);
+                }
+
+                if (position == 5) {
+                    Toast.makeText(sg552.this, "Weapon Selected FS2000", Toast.LENGTH_SHORT).show();
+                    Intent fs2000 = new Intent(getApplicationContext(), fs2000.class);
+                    startActivity(fs2000);
+                }
+                if (position == 6) {
+                    Toast.makeText(sg552.this, "Weapon Selected G36C", Toast.LENGTH_SHORT).show();
+                    Intent g36c = new Intent(getApplicationContext(), g36c.class);
+                    startActivity(g36c);
+                }
+                if (position == 7) {
+                    Toast.makeText(sg552.this, "Weapon Selected SG552", Toast.LENGTH_SHORT).show();
+                    Intent sg552 = new Intent(getApplicationContext(), sg552.class);
+                    startActivity(sg552);
+                }
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+
+        });
+
+
+    }
+    public void Reweapon8(View v) {//Перезарядка
+        OurSoundPlayer.playSound(this, OurSoundPlayer.S3, 0);
+        patron = 30;
+        patron1.setText(String.valueOf(patron));
+        try {
+            Thread.sleep(3500);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (timer == null) {
+                timer = new Timer();
+                timer.scheduleAtFixedRate(new UpdateTimeTask9(), 50, 200);
+            }
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            timer.cancel();
+            timer = null;
+        }
+        if (patron == 0)
+            Allmusicgun.OurSoundPlayer.playSound(getApplicationContext(), Allmusicgun.OurSoundPlayer.S2, 0);
+        return true;
+    }
+    class UpdateTimeTask9 extends TimerTask {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if ((patron <= 30) && (patron > 0)) {
+                        Allmusicgun.OurSoundPlayer.playSound(getApplicationContext(), Allmusicgun.OurSoundPlayer.S14, 0);
+                        patron = patron - 1;
+                        patron1.setText(String.valueOf(patron));
+                    }
+                }
+
+            });
+        }
+
+    }
+    public void backsg552(View view) {
+        Intent intent = new Intent(getApplicationContext(), m4a1.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.twotoone, R.anim.twotoonee);
+    }
+
+    public void sg552go(View view) {
+        Intent intent = new Intent(this, ak47.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.onetotwo, R.anim.onetwoo);
+    }
+}
